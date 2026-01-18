c9154f5cc3b1229c6576885df8165146f398ccbc
exi66 <eternityexi@gmail.com>
2026-01-18 14:56:16 +0400

    fix tar dep

--- webpack.common.js
+++ webpack.common.js
@@ -1,63 +1,66 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const output_path = path.resolve(__dirname, "dist");
 module.exports = {
     entry: "./src/js/index.js",
     optimization: {
-        usedExports: true
+        usedExports: true,
     },
     output: {
         filename: "timeline.js",
-        path: path.join(output_path, 'js'),
-        library: "TL" // https://webpack.js.org/configuration/output/#outputlibrary
+        path: path.join(output_path, "js"),
+        library: "TL", // https://webpack.js.org/configuration/output/#outputlibrary
     },
     plugins: [
         new CopyPlugin({
-            patterns: [{
+            patterns: [
+                {
                     from: "./src/js/language/locale/*.json",
-                    to: path.join(output_path, "js/locale"),
-                    flatten: true
+                    to: path.join(output_path, "js/locale/[name][ext]"),
                 },
                 {
-                    from: './src/embed/*',
-                    to: path.join(output_path, "embed"),
-                    flatten: true
-                }
-            ]
+                    from: "./src/embed/*",
+                    to: path.join(output_path, "embed/[name][ext]"),
+                },
+            ],
         }),
         new CleanWebpackPlugin({
-            cleanStaleWebpackAssets: true
+            cleanStaleWebpackAssets: true,
         }),
     ],
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.less$/,
-                use: [{
-                        loader: 'css-loader',
+                use: [
+                    {
+                        loader: "css-loader",
                         options: {
                             sourceMap: true,
-                        }
+                        },
                     },
                     {
-                        loader: 'less-loader',
+                        loader: "less-loader",
                         options: {
                             sourceMap: true,
-                        }
+                        },
                     },
                 ],
             },
             {
                 test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '../css/icons'
-                    }
-                }]
-            }
-        ]
-    }
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "../css/icons",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
